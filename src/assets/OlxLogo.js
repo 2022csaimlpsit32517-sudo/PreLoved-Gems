d06c7f381f960829c62e4332ee1a5008451fc85f
2022csaimlpsit32517-sudo <2022.csaiml.psit.32517@gmail.com>
2026-06-13 10:10:55 +0530

    Update OlxLogo.js

--- src/assets/OlxLogo.js
+++ src/assets/OlxLogo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OlxLogo() {
   return (
     <>
-    <img src="https://firebasestorage.googleapis.com/v0/b/olx-sijeesh-c691f.appspot.com/o/image%2Fthrift-shop%20(2).png?alt=media&token=a0fcd3f3-1667-4ffc-bb4e-1efd17ccee4b" width="48px" height="48px"/>
+    <img src="/olx-logo.png" width="48px" height="48px"/>
     </>
   )
 }
